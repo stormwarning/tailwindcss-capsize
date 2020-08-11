1b9235d9afc238daca7492f6983323303527d6bc
Jeff <rustyangel@gmail.com>
2020-08-11 10:01:21 -0600

    Add normalizeValue utility

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,84 @@
 import capsize from 'capsize'
-import { TailwindPlugin } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 
-import { makeCssSelectors, getValueAndUnit } from './utils'
+import { makeCssSelectors, normalizeValue } from './utils'
 
-// eslint-disable-next-line
-// @ts-ignore
-export default plugin(function ({ addUtilities, theme }) {
-    /** @todo Get root font size from plugin config */
+interface FontMetrics {
+    ascent: number
+    descent: number
+    lineGap: number
+    unitsPerEm: number
+    capHeight: number
+}
 
-    let fontMetrics = theme('fontMetrics', {})
-    let lineHeight = theme('lineHeight', {})
-    let fontSize = theme('fontSize', {})
+interface CapsizeStyles {
+    fontSize: string
+    lineHeight: string
+    padding: string
+    '::before': {
+        content: string
+        marginTop: string
+        display: string
+        height: number
+    }
+    '::after': {
+        content: string
+        marginBottom: string
+        display: string
+        height: number
+    }
+}
 
-    let remLeadings = Object.keys(lineHeight).filter((leading) =>
-        lineHeight[leading].endsWith('rem'),
-    )
+interface PluginOptions {
+    /** The root font-size, in pixels */
+    rootSize?: number
+}
 
-    let utilities = {} as { [property: string]: any }
+export default plugin.withOptions(function (options: PluginOptions) {
+    let { rootSize } = options
 
-    Object.keys(fontMetrics).forEach((fontFamily) => {
-        let fontConfig = fontMetrics[fontFamily]
+    return function ({ addUtilities, theme }) {
+        let fontMetrics: FontMetrics = theme('fontMetrics', {})
+        let lineHeight = theme('lineHeight', {})
+        let fontSize = theme('fontSize', {})
 
-        Object.keys(fontSize).forEach((sizeName) => {
-            remLeadings.forEach((leading) => {
-                /** @todo Strip unit and convert to px */
-                let [lineHeightRem] = getValueAndUnit(lineHeight[leading])
+        let remLeadings = Object.keys(lineHeight).filter((leading) =>
+            lineHeight[leading].endsWith('rem'),
+        )
 
-                utilities[
-                    makeCssSelectors(fontFamily, sizeName, leading)
-                ] = capsize({
-                    fontMetrics: fontConfig,
-                    fontSize,
-                    leading: lineHeightRem,
+        let utilities = {} as { [property: string]: any }
+
+        Object.keys(fontMetrics).forEach((fontFamily) => {
+            let fontConfig = fontMetrics[fontFamily]
+
+            Object.keys(fontSize).forEach((sizeName) => {
+                remLeadings.forEach((leading) => {
+                    /** @todo Strip unit and convert to px */
+                    let fs = normalizeValue(fontSize[sizeName], rootSize)
+                    let lh = normalizeValue(remLeadings[leading], rootSize)
+
+                    utilities[
+                        makeCssSelectors(fontFamily, sizeName, leading)
+                    ] = capsize({
+                        fontMetrics: fontConfig,
+                        fontSize: fs,
+                        leading: lh,
+                    })
                 })
             })
         })
-    })
 
-    // utilities['.baseline-debug-stripe'] = {
-    //     background: `repeating-linear-gradient(0, #68d391,#68d391 ${gridRowHeightRem}rem, #c6f6d5 ${gridRowHeightRem}rem, #c6f6d5 ${
-    //         2 * gridRowHeightRem
-    //     }rem)`,
-    // }
-    // utilities['.baseline-debug-line'] = {
-    //     background:
-    //         'linear-gradient(rgba(255, 0, 0, 0.15), rgba(255, 0, 0, 0.15) 1px, transparent 1px)',
-    //     backgroundSize: `1px ${gridRowHeightRem}rem`,
-    // }
+        // utilities['.baseline-debug-stripe'] = {
+        //     background: `repeating-linear-gradient(0, #68d391,#68d391 ${gridRowHeightRem}rem, #c6f6d5 ${gridRowHeightRem}rem, #c6f6d5 ${
+        //         2 * gridRowHeightRem
+        //     }rem)`,
+        // }
+        // utilities['.baseline-debug-line'] = {
+        //     background:
+        //         'linear-gradient(rgba(255, 0, 0, 0.15), rgba(255, 0, 0, 0.15) 1px, transparent 1px)',
+        //     backgroundSize: `1px ${gridRowHeightRem}rem`,
+        // }
 
-    addUtilities(utilities)
-}) as TailwindPlugin
+        addUtilities(utilities)
+    }
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
+export function normalizeValue(value: string, root?: number): number {
+    if (!root) root = 16
+
+    let isPixelValue = value.endsWith('px')
+    let isRemValue = value.endsWith('rem')
+
+    if (isPixelValue) return parseInt(value.replace('px', ''))
+    if (isRemValue) return root * parseFloat(value.replace('rem', ''))
+
+    return parseInt(value)
+}
+
 const cssRegex = /^([+-]?(?:\d+|\d*\.\d+))([a-z]*|%)$/
 
 export function getValueAndUnit(value: string): [number, string | undefined] {
