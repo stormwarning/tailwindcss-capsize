513d3923162e70126f21692e30220ea2eb9d7c3e
Jeff <rustyangel@gmail.com>
2021-09-20 14:57:13 -0600

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
 module.exports = {
-    extends: ['@zazen/eslint-config'],
-    plugins: ['@typescript-eslint'],
-    rules: {},
-    overrides: [
-        {
-            files: ['**/*.ts'],
-            extends: [
-                '@zazen/eslint-config',
-                // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
-                'plugin:@typescript-eslint/recommended',
-                // https://github.com/prettier/eslint-config-prettier/blob/master/%40typescript-eslint.js
-                'prettier/@typescript-eslint',
-            ],
-            parser: '@typescript-eslint/parser',
-            env: {
-                node: true,
-            },
-            rules: {
-                'prefer-const': 'off',
-            },
-        },
-    ],
+    extends: ['@zazen/eslint-config/typescript'],
+    parserOptions: {
+        project: null,
+    },
+    env: {
+        node: true,
+    },
 }
